b8ee1a736633f7ec937eb1a041ac50cd77a9766f
Seungho Choi <anteaterho@gmail.com>
2015-10-10 23:10:30 +0900

    Init

---
+++ Source/UE4Study/UE4StudyGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Study.h"
+#include "UE4StudyGameMode.h"
+
+
+
+
---
+++ Source/UE4Study/UE4StudyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UE4StudyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4STUDY_API AUE4StudyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
